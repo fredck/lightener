5d90c53f9c4329508f23809802e3b18d7774e0d6
fredck <fredck@fredck.com>
2023-05-17 19:15:34 +0000

    Added support for nested lighteners

--- tests/components/lightener/conftest.py
+++ tests/components/lightener/conftest.py
@@ -2,14 +2,22 @@
 
 import pytest
 
+from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
 
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
     yield
 
+@pytest.fixture(autouse=True)
+async def setup_components(hass: HomeAssistant):
+    """Register test lights used in tests"""
+
+    await async_setup_component(hass,"homeassistant", {})
+    await async_setup_component(hass,"light", {})
 
 @pytest.fixture(autouse=True)
-def register_test_lights(hass):
+def register_test_lights(hass: HomeAssistant):
     """Register test lights used in tests"""
 
     hass.states.async_set(entity_id="light.test1", new_state="off")
