1b11f167a039f55aa3579125ffe0ce41b9eda2e4
fredck <fredck@fredck.com>
2023-12-20 16:19:42 +0000

    Add tests for async_setup_entry and async_unload_entry

--- tests/components/lightener/test_init.py
+++ tests/components/lightener/test_init.py
@@ -3,12 +3,63 @@
 import logging
 from unittest.mock import Mock, patch
 
-from homeassistant.config_entries import ConfigEntry
+from homeassistant.config_entries import ConfigEntry, ConfigEntryState
 from homeassistant.core import HomeAssistant
+from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.lightener import async_migrate_entry
+from custom_components.lightener import (
+    async_migrate_entry,
+    async_setup_entry,
+    async_unload_entry,
+)
 from custom_components.lightener.config_flow import LightenerConfigFlow
 from custom_components.lightener.const import DOMAIN
+
+
+async def test_async_setup_entry(hass):
+    """Test setting up Lightener successfully."""
+    config_entry = MockConfigEntry(
+        domain="lightener",
+        data={
+            "friendly_name": "Test",
+            "entities": {
+                "light.test1": {},
+            },
+        },
+    )
+    config_entry.add_to_hass(hass)
+
+    assert await async_setup_entry(hass, config_entry)
+    await hass.async_block_till_done()
+    assert "light.lightener" in hass.config.components
+
+
+@patch("custom_components.lightener.async_unload_entry", wraps=async_unload_entry)
+async def test_async_unload_entry(mock_unload, hass):
+    """Test setting up Lightener successfully."""
+    config_entry = MockConfigEntry(
+        domain="lightener",
+        data={
+            "friendly_name": "Test",
+            "entities": {
+                "light.test1": {},
+            },
+        },
+    )
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+    assert config_entry.state is ConfigEntryState.LOADED
+    assert "light.test" in hass.states.async_entity_ids()
+
+    assert await hass.config_entries.async_remove(config_entry.entry_id)
+    await hass.async_block_till_done()
+    assert config_entry.state is ConfigEntryState.NOT_LOADED
+    assert "light.test" not in hass.states.async_entity_ids()
+
+    # Ensure that the Lightener unload implementation was called.
+    mock_unload.assert_called_once()
+    assert mock_unload.return_value
 
 
 async def test_migrate_entry_current(hass: HomeAssistant) -> None:
